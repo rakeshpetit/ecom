26d5c3bb40d2fea2b877c0d259520f5819e4e365
Rakesh <rakeshpetit@gmail.com>
2018-08-10 15:43:32 +0530

    Header region change

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ProductsMenu from './menus/ProductsMenu';
+import './Header.css'; 
+import logo from '../assets/Logo.png';
 
 const Header = () => {
   return (
@@ -10,7 +12,10 @@ const Header = () => {
           <ul style={ulStyle} >
             <li style={liStyle}><Link to={'/about'} className="nav-link">About</Link></li>
             <li style={liStyle}><Link to={'/products'} className="nav-link"><ProductsMenu/></Link></li>
-            <li style={liStyle}><Link to={'/'} className="nav-link">Home</Link></li>
+            <li style={imgLiStyle}>
+            <Link to={'/'} className="nav-link">
+              <img style={imgStyle} src={logo} alt="Tannaaz"/>
+            </Link></li>
             <li style={liStyle}><Link to={'/contact'} className="nav-link">Contact</Link></li>
             <li style={liStyle}><Link to={'/search'} className="nav-link">Search</Link></li>
           </ul>
@@ -25,13 +30,27 @@ const navStyle = {
 
 const ulStyle = {
   display: 'flex',
-  margin: '0 25%',
+  margin: '0 20%',
   listStyle: 'none',
-  justifyContent: 'space-around'
+  alignItems: 'flex-end',
+  justifyContent: 'space-between'
 };
 
-const liStyle = {
+const imgStyle = {
+  width: '50%'
+};
+
+const liStyle = {  
+  width: '20%',
   display: 'inline'
 };
+
+const imgLiStyle = {
+  width: '20%',
+  paddingTop: '10px',
+  display: 'inline'
+};
+
+
 
 export default Header;
